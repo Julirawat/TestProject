53c1d85d33f74ab5be3975b9ad3cfef20e4a1ddb
rawat <rawat@Hp>
2023-07-09 19:23:28 +0530

    first commit

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+	int n1=5, n2=2;
+	public void add()
+	{
+		System.out.println("Sum is " +(n1+n2));
+	}
+	public static void main(String a[])
+	{
+		Calculator calculator=new Calculator();
+		calculator.add();
+	}
+
+}
